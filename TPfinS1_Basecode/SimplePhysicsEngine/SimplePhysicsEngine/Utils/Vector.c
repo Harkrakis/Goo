02ab5673a310c48e2b404c3b0be1f85b1cb0f402
Alexis GARREAU <spam.manager@yahoo.com>
2022-01-04 14:11:54 +0100

    vecteurs

--- TPfinS1_Basecode/SimplePhysicsEngine/SimplePhysicsEngine/Utils/Vector.c
+++ TPfinS1_Basecode/SimplePhysicsEngine/SimplePhysicsEngine/Utils/Vector.c
@@ -1,5 +1,5 @@
-﻿#include "Vector.h"
-
+﻿#include "vector.h"
+#include <math.h>
 Vec2 Vec2_Set(float x, float y)
 {
     Vec2 v = { .x = x, .y = y };
@@ -15,30 +15,41 @@ Vec2 Vec2_Perp(Vec2 v)
 
 Vec2 Vec2_Add(Vec2 v1, Vec2 v2)
 {
+    v1.x+=v2.x;
+    v1.y+=v2.y;
     return v1;
 }
 
 Vec2 Vec2_Sub(Vec2 v1, Vec2 v2)
 {
+    v1.x-=v2.x;
+    v1.y-=v2.y;
     return v1;
 }
 
 Vec2 Vec2_Scale(Vec2 v, float s)
 {
+    v.x*=s;
+    v.y*=s;
     return v;
 }
 
 float Vec2_Length(Vec2 v)
 {
-    return 0.0f;
+    
+    return sqrt(v.x*v.x+v.y*v.y);
 }
 
 Vec2 Vec2_Normalize(Vec2 v)
 {
+    float tmp = Vec2_Length(v);
+    v.x=v.x / tmp;
+    v.y=v.y/ tmp;
     return v;
 }
 
 float Vec2_Distance(Vec2 v1, Vec2 v2)
 {
-    return 0.0f;
+    return Vec2_Length(Vec2_Sub(v1,v2));
 }
+
